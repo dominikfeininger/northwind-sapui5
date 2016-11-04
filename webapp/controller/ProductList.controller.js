e2883ea7f9077d579d5cc3a871d5a3378bcc14fa
kpbitson <kbitson@icloud.com>
2016-11-04 16:09:48 -0700

    removed comments and console statements

--- webapp/controller/ProductList.controller.js
+++ webapp/controller/ProductList.controller.js
@@ -28,19 +28,7 @@ sap.ui.define([
 		
 		onClear: function(oEvent) {
 			var oView = this.getView(),
-				oBinding = oView.byId("productList").getBinding("items"),
-				aFilterItems = oView.byId("filterBar").getFilterItems(),
-				aInputs = [];
-			for (var i = 0; i < aFilterItems.length; i++) {
-				aInputs.push(aFilterItems[i].getId());
-				$(aInputs[i]).html("");
-				// oView.byId( aInputs[i] ).innerHTML("");
-			}
-			// if (aInputs.length > 0) {
-			// 	for (var j = 0, j < aInputs.length; j++) {
-			// 		console.log(aInputs);
-			// 	}
-			// }
+				oBinding = oView.byId("productList").getBinding("items");
 			oBinding.filter(null);
 		},
 

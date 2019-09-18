e239f0c103893edb9658e8674e502802a92cdf12
dominik feininger <dominik.feininger@gmail.com>
2019-09-18 12:32:46 +0200

    add "add product" function

--- webapp/controller/ProductList.controller.js
+++ webapp/controller/ProductList.controller.js
@@ -13,6 +13,37 @@ sap.ui.define([
 			});
 			this.getView().setModel(oViewModel, "view");
 		},
+		
+		onAddProductPress: function (oEvent) {
+
+		    var oView = this.getView();
+		    var oFilterBar = oView.byId("filterBar");
+		    var oFilterItems = oFilterBar.getAllFilterItems(true);
+		    var sProductName = oFilterBar.determineControlByFilterItem(oFilterItems[0]).getValue();
+		    var sCategoryName = oFilterBar.determineControlByFilterItem(oFilterItems[1]).getValue();
+		    var sSupplierName = oFilterBar.determineControlByFilterItem(oFilterItems[2]).getValue();
+
+		    // OPTION 1:
+		    // create an entry of the Products collection with the specified properties and values
+		    var oContext = this.getView().getModel("northwind").createEntry("/Products",
+			{
+			    properties: {
+				ProductName: sProductName,
+				// multiple propertis follow
+			    }
+			});
+		    this.getView().getModel("northwind").submitChanges();
+
+		    /*
+		    // // OPTION 2:
+		    var oData = {
+			ProductName: sProductName
+			// multiple propertis follow
+		    };
+
+		    this.getView().getModel("northwind").create("/Products", oData);
+		    */
+		},
 
 		onSearch: function() {
 			var aFilters = [],
